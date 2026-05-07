f504054d13d3bf1f7c1540ddb4870948e843f67e
jay730 <jayesh.neupane@gmail.com>
2026-05-07 17:06:39 -0500

    Extended BaseService

--- backend/src/services/staffService.ts
+++ backend/src/services/staffService.ts
@@ -2,37 +2,17 @@ import { staff as initialStaff } from "../data/staff";
 import { Staff, StaffRole } from "../types/staff";
 import { BaseService } from "./baseService";
 
-export class StaffService {
-  private staff: Staff[] = [...initialStaff];
-  getAllStaff = (): Staff[] => {
-    return this.staff;
-  };
-
-  getStaffById = (id: number): Staff | undefined => {
-    for (const staffMember of this.staff) {
-      if (staffMember.id === id) {
-        return staffMember;
-      }
-    }
-    return undefined;
-  };
-
-  createStaff = (newStaff: Staff): Staff | string => {
-    for (const staffMember of this.staff) {
-      if (staffMember.id === newStaff.id) {
-        return "Staff with the same ID already exists";
-      }
-    }
-    newStaff.role = newStaff.role ?? StaffRole.Caregiver;
-    this.staff.push(newStaff);
-    return newStaff;
-  };
+export class StaffService extends BaseService<Staff> {
+ constructor() {
+     super();
+     this.items = [...initialStaff];
+   }
 
   updateStaff = (
     id: number,
     updateData: Partial<Omit<Staff, "id">>,
   ): Staff | undefined => {
-    for (const staffMember of this.staff) {
+    for (const staffMember of this.items) {
       if (staffMember.id === id) {
         Object.assign(staffMember, updateData);
         return staffMember;
@@ -41,30 +21,20 @@ export class StaffService {
     return undefined;
   };
 
-  deleteStaff = (id: number): boolean => {
-    for (let i = 0; i < this.staff.length; i++) {
-      if (this.staff[i]?.id === id) {
-        this.staff.splice(i, 1);
-        return true;
-      }
-    }
-    return false;
-  };
-
   getStaffByRole = (role: StaffRole, supervisorId?: number): Staff[] => {
     const selectedStaff = [];
     if (supervisorId) {
-      for (const staffMember of this.staff) {
+      for (const staffMember of this.items) {
         if (
           staffMember.role == role &&
           staffMember.supervisorId == supervisorId
-        ) {
+        ) { 
           selectedStaff.push(staffMember);
         }
       }
       return selectedStaff;
     } else {
-      for (const staffMember of this.staff) {
+      for (const staffMember of this.items) {
         if (staffMember.role == role) {
           selectedStaff.push(staffMember);
         }
@@ -78,16 +48,16 @@ const service = new StaffService();
 
 console.log(
   1,
-  service.createStaff({ id: 1, name: "John", role: StaffRole.Nurse }),
+  service.create({ id: 1, name: "John", role: StaffRole.Nurse }),
 );
-console.log(2, service.getAllStaff());
-console.log(3, service.getStaffById(1));
+console.log(2, service.getAll());
+console.log(3, service.getById(1));
 console.log(4, service.updateStaff(1, { name: "Jane" }));
-console.log(5, service.deleteStaff(3));
+console.log(5, service.delete(3));
 console.log(
   6,
-  service.createStaff({ id: 4, name: "John", role: StaffRole.Nurse }),
+  service.create({ id: 4, name: "John", role: StaffRole.Nurse }),
 );
-console.log(7, service.deleteStaff(99));
-console.log(6, service.createStaff({ id: 3, name: "Jackie" }));
-console.log(9, service.getAllStaff());
+console.log(7, service.delete(99));
+console.log(6, service.create({ id: 3, name: "Jackie" }));
+console.log(9, service.getAll());
--- backend/src/services/taskService.ts
+++ backend/src/services/taskService.ts
@@ -1,37 +1,18 @@
 import { tasks as initialTasks } from "../data/tasks";
 import { Task } from "../types/task";
+import { BaseService } from "./baseService";
 
-export class TaskService {
-  private tasks: Task[] = [...initialTasks];
-
-  getAllTask = (): Task[] => {
-    return this.tasks;
-  };
-
-  getTaskById = (id: number): Task | undefined => {
-    for (const taskMember of this.tasks) {
-      if (taskMember.id === id) {
-        return taskMember;
-      }
-    }
-    return undefined;
-  };
-
-  createTask = (newTask: Task): Task | string => {
-    for (const taskMember of this.tasks) {
-      if (taskMember.id === newTask.id) {
-        return "Task with the same ID already exists";
-      }
-    }
-    this.tasks.push(newTask);
-    return newTask;
-  };
+export class TaskService extends BaseService<Task> {
+  constructor() {
+       super();
+       this.items = [...initialTasks];
+     }
 
   updateTask = (
     id: number,
     updateData: Partial<Omit<Task, "id">>,
   ): Task | undefined => {
-    for (const taskMember of this.tasks) {
+    for (const taskMember of this.items) {
       if (taskMember.id === id) {
         Object.assign(taskMember, updateData);
         return taskMember;
@@ -40,36 +21,27 @@ export class TaskService {
     return undefined;
   };
 
-  deleteTask = (id: number): boolean => {
-    for (let i = 0; i < this.tasks.length; i++) {
-      if (this.tasks[i]?.id === id) {
-        this.tasks.splice(i, 1);
-        return true;
-      }
-    }
-    return false;
-  };
 }
 
 const service = new TaskService();
 
 console.log(
   1,
-  service.createTask({
+  service.create({
     id: 1,
     name: "x",
     residentId: 1,
     description: "Ambulation",
   }),
 );
-console.log(2, service.getAllTask());
-console.log(3, service.getTaskById(1));
+console.log(2, service.getAll());
+console.log(3, service.getById(1))
 console.log(4, service.updateTask(1, { description: "Jane" }));
-console.log(5, service.deleteTask(3));
+console.log(5, service.delete(3));
 console.log(
   6,
-  service.createTask({ id: 4, name: "y", description: "Ambulation" }),
+  service.create({ id: 4, name: "y", description: "Ambulation" }),
 );
-console.log(7, service.deleteTask(99));
-console.log(6, service.createTask({ id: 3, name: "z", description: "Ad hoc" }));
-console.log(9, service.getAllTask());
+console.log(7, service.delete(99));
+console.log(6, service.create({ id: 3, name: "z", description: "Ad hoc" }));
+console.log(9, service.getAll());
